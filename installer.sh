d10ad6c4b1783c25f50cfb05e74634d74be44eb1
王逸涵 <166415175+henry5450@users.noreply.github.com>
2026-03-15 18:17:39 +0800

    Add installer script for Rish in Termux

---
+++ installer.sh
@@ -0,0 +1,4 @@
+mkdir -p /data/data/com.termux/files/home/storage/Shizuku/
+wget -O /data/data/com.termux/files/usr/bin/rish https://raw.githubusercontent.com/henry5450/rish-for-Termux/main/rish
+wget -O /data/data/com.termux/files/home/storage/Shizuku/rish_shizuku.dex https://raw.githubusercontent.com/henry5450/rish-for-Termux/main/rish_shizuku.dex
+chmod 755 /data/data/com.termux/files/usr/bin/rish
